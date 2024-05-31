5743fef85c86f128090f42494f90f7200a2181a9
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2024-05-31 08:46:54 +0200

    changed image fields for char fields

--- filmy/models.py
+++ filmy/models.py
@@ -5,9 +5,8 @@ class Movie(models.Model):
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="in minutes")
     description = models.TextField(blank=True)
-    main_picture = models.ImageField(blank=True, null=True)
-    director = models.ForeignKey('Director'
-    , blank=True, null=True, on_delete=models.SET_NULL)
+    main_picture = models.CharField(max_length=2048, blank=True, null=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
     actors = models.ManyToManyField('Actor', blank=True)
     genres = models.ManyToManyField('Genre', blank=True)
 
@@ -21,7 +20,7 @@ class Director(models.Model):
     name = models.CharField(max_length=300)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(max_length=2048, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -31,7 +30,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=300)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(max_length=2048, blank=True, null=True)
 
     def __str__(self):
         return self.name
